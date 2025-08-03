646809ace07f1613505ea82530286143562350af
Allison Tielking <hyperallison@gmail.com>
2025-08-03 21:11:21 +0300

    reorg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,25 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <div className="font-sans items-start justify-items-start min-h-screen p-8 sm:p-20">
-      <div>
-        <h1>Allison Tielking</h1>
-        <div className="flex gap-2">        
-          <div><h2>software engineer & city planner</h2></div>
-          <div>{'\u00B7'}</div>
-          <div><a href="mailto:hi@allisontielking.com">hi@allisontielking.com</a></div>
+    return (
+        <div className="min-h-screen px-20 py-10 flex flex-col justify-between">
+            <div>
+                <h1>Allison Tielking</h1>
+                <h3>software engineer & city planner in training</h3>
+            </div>
+            <div className="flex items-center justify-center gap-2">
+                <div>
+                    <a href="mailto:hi@allisontielking.com">email</a>
+                </div>
+                <div>{"\u00B7"}</div>
+                <div>
+                    <a href="https://www.linkedin.com/in/allison-tielking/">
+                        linkedin
+                    </a>
+                </div>
+                <div>{"\u00B7"}</div>
+                <div>
+                    <a href="https://github.com/atielking98">github</a>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
